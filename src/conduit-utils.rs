4a4f3b0c7b134c159d0bc0ec45cedf5e2ef19653
Yehuda Katz <wycats@gmail.com>
2014-07-04 02:50:04 -0700

    Add HeaderMap
    It is possible that this should be part of the raw Conduit protocol to
    avoid unnecessary forced normalization.

--- src/conduit-utils.rs
+++ src/conduit-utils.rs
@@ -4,7 +4,9 @@
 extern crate semver;
 extern crate conduit;
 
+use std::iter;
 use std::io::net::ip::IpAddr;
+use std::collections::hashmap::{HashMap, Entries};
 
 use conduit::{Method, Scheme, Host, Extensions, Headers, Request};
 
@@ -127,12 +129,78 @@ impl<'a> Request for &'a mut RequestDelegator {
     }
 }
 
+type RawHeaders = HashMap<String, Vec<String>>;
+type InHeader<'a> = (&'a String, &'a Vec<String>);
+type OutHeader<'a> = (String, &'a Vec<String>);
+
+#[deriving(PartialEq, Clone, Show)]
+pub struct HeaderMap(HashMap<String, Vec<String>>);
+
+impl HeaderMap {
+    fn normalize(headers: HashMap<String, Vec<String>>) -> HeaderMap {
+        let headers = headers.move_iter().map(|(k,v)| (to_lower(&k), v)).collect();
+        HeaderMap(headers)
+    }
+
+    fn iter<'a>(&'a self) -> iter::Map<'a, InHeader<'a>, OutHeader<'a>, Entries<'a, String, Vec<String>>> {
+        self.as_ref().iter().map(|(k,v)| (to_lower(k), v))
+    }
+
+    fn as_ref<'a>(&'a self) -> &'a HashMap<String, Vec<String>> {
+        match *self {
+            HeaderMap(ref map) => map
+        }
+    }
+
+    fn as_mut<'a>(&'a mut self) -> &'a mut HashMap<String, Vec<String>> {
+        match *self {
+            HeaderMap(ref mut map) => map
+        }
+    }
+}
+
+impl Collection for HeaderMap {
+    fn len(&self) -> uint {
+        self.as_ref().len()
+    }
+}
+
+impl Mutable for HeaderMap {
+    fn clear(&mut self) {
+        self.as_mut().clear()
+    }
+}
+
+impl<S: Str> Map<S, Vec<String>> for HeaderMap {
+    fn find<'a>(&'a self, key: &S) -> Option<&'a Vec<String>> {
+        self.as_ref().find(&to_lower(key))
+    }
+}
+
+impl<S: Str> MutableMap<S, Vec<String>> for HeaderMap {
+    fn swap(&mut self, k: S, v: Vec<String>) -> Option<Vec<String>> {
+        self.as_mut().swap(to_lower(&k), v)
+    }
+
+    fn pop(&mut self, k: &S) -> Option<Vec<String>> {
+        self.as_mut().pop(&to_lower(k))
+    }
+
+    fn find_mut<'a>(&'a mut self, k: &S) -> Option<&'a mut Vec<String>> {
+        self.as_mut().find_mut(&to_lower(k))
+    }
+}
+
+fn to_lower<S: Str>(string: &S) -> String {
+    string.as_slice().chars().map(|c| c.to_lowercase()).collect()
+}
 
 #[cfg(test)]
 mod tests {
     extern crate test = "conduit-test";
     use super::*;
 
+    use std::collections::HashMap;
     use conduit;
     use conduit::{Request, Method};
 
@@ -161,5 +229,52 @@ mod tests {
 
         assert_eq!(override.method(), conduit::Get);
         assert_eq!(override.path(), "/hello");
+    }
+
+    #[test]
+    fn test_header_map() {
+        let mut map = HeaderMap(HashMap::new());
+        map.insert("Content-Type".to_str(), vec!("text/html".to_str()));
+        map.insert("location".to_str(), vec!("http://example.com".to_str()));
+
+        assert_eq!(map.find(&"content-type".to_str()), Some(&vec!("text/html".to_str())))
+        assert_eq!(map.find(&"Location".to_str()), Some(&vec!("http://example.com".to_str())))
+        assert_eq!(map.find(&"content-type"), Some(&vec!("text/html".to_str())))
+        assert_eq!(map.find(&"Location"), Some(&vec!("http://example.com".to_str())))
+    }
+
+    #[test]
+    fn test_header_map_with_static_inserts() {
+        let mut map = HeaderMap(HashMap::new());
+        map.insert("Content-Type", vec!("text/html".to_str()));
+        map.insert("location", vec!("http://example.com".to_str()));
+
+        assert_eq!(map.find(&"content-type".to_str()), Some(&vec!("text/html".to_str())))
+        assert_eq!(map.find(&"Location".to_str()), Some(&vec!("http://example.com".to_str())))
+        assert_eq!(map.find(&"content-type"), Some(&vec!("text/html".to_str())))
+        assert_eq!(map.find(&"Location"), Some(&vec!("http://example.com".to_str())))
+    }
+
+    #[test]
+    fn test_normalize() {
+        let mut map = HashMap::new();
+        map.insert("Content-Type".to_str(), vec!("text/html".to_str()));
+
+        let headers = HeaderMap::normalize(map);
+        assert_eq!(headers.find(&"Content-Type".to_str()), Some(&vec!("text/html".to_str())))
+        assert_eq!(headers.find(&"Content-Type"), Some(&vec!("text/html".to_str())))
+        assert_eq!(headers.find(&"content-type".to_str()), Some(&vec!("text/html".to_str())))
+        assert_eq!(headers.find(&"content-type"), Some(&vec!("text/html".to_str())))
+    }
+
+    #[test]
+    fn test_iterate() {
+        let mut headers = HeaderMap(HashMap::new());
+        headers.insert("Content-Type", vec!("text/html".to_str()));
+        headers.insert("location", vec!("http://example.com".to_str()));
+
+        assert!(headers.iter().any(|t| t == ("content-type".to_str(), &vec!("text/html".to_str()))));
+        assert!(headers.iter().any(|t| t == ("location".to_str(), &vec!("http://example.com".to_str()))));
+        assert!(headers.iter().count() == 2);
     }
 }
